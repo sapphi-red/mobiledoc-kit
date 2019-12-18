78bbdaa432e787e926eb00f2379717ec01d0b005
huaying <royal3501@gmail.com>
2019-12-19 03:42:29 +0800

    fix ime issue

--- src/js/renderers/editor-dom.js
+++ src/js/renderers/editor-dom.js
@@ -503,7 +503,7 @@ let destroyHooks = {
       marker.section.markers.remove(marker);
     }
 
-    if (markupElement.parentNode) {
+    if (markupElement && markupElement.parentNode) {
       // if no parentNode, the browser already removed this element
       markupElement.parentNode.removeChild(markupElement);
     }
